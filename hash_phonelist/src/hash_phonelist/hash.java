d8409c172c5b884b6a1b23d3577e1b43e6c7e1d1
saif-yousry <167723545+saif-yousry@users.noreply.github.com>
2024-12-02 00:29:14 +0200

    Update hash.java

--- hash_phonelist/src/hash_phonelist/hash.java
+++ hash_phonelist/src/hash_phonelist/hash.java
@@ -1,35 +1,33 @@
 package hash_phonelist;
 public class hash {
-    private final int capacity;
+    private int capacity;
     private int size;
-    private int removecounter;
-    private final HashNode[] Contacts;
-    public hash(int cap){
-        capacity=cap;
+    private HashNode[] Contacts;
+    public hash(int cap) {
+        capacity = cap;
         this.Contacts = new HashNode[capacity];
-        this.size=0;
+        this.size = 0;
     }
-    public static class HashNode{
+    public static class HashNode {
         Node node;
-        boolean occupiedBefore=false;
-
-        public HashNode(String name, String phone){
+        boolean occupiedBefore = false;
+        public HashNode(String name, String phone) {
             this.node = new Node(name, phone);
-            this.occupiedBefore= true;
+            this.occupiedBefore = true;
         }
-        public static class Node{
+        public static class Node {
             String name;
             String phone;
-            Node(String n, String ph){
-                name= n;
-                phone= ph;
+            Node(String n, String ph) {
+                name = n;
+                phone = ph;
             }
         }
     }
     private long hash_calc(String key) {
-        int i, l = key.length();
+        int l = key.length();
         long hash = 0;
-        for (i = 0; i < l; i++) {
+        for (int i = 0; i < l; i++) {
             hash += Character.getNumericValue(key.charAt(i));
             hash += (hash << 10);
             hash ^= (hash >> 6);
@@ -37,114 +35,113 @@ public class hash {
         hash += (hash << 3);
         hash ^= (hash >> 11);
         hash += (hash << 15);
-
         if (hash > 0) return hash % capacity;
         else return -hash % capacity;
     }
-    private boolean isFull(){
-        if(size == capacity){
-            return true;
-        }else
-            return false;
-        
+    private boolean isFull() {
+        return size == capacity;
     }
-    public boolean isEmpty(){
-        if(size == 0)
-            return true;
-        else
-            return false;
+    public boolean isEmpty() {
+        return size == 0;
     }
-    public int size(){
+    public int size() {
         return size;
     }
-    public void insert(String name,String phone){
-        if(isFull()==true){
-          System.out.println("The contact list is full.");
-          return;
+    private void rehash() {
+        System.out.println("Rehashing...");
+        int oldCapacity = capacity;
+        HashNode[] oldContacts = Contacts;
+        capacity *= 2;
+        Contacts = new HashNode[capacity];
+        size = 0;
+        for (HashNode node : oldContacts) {
+            if (node != null && node.node != null) {
+                insert(node.node.name, node.node.phone);
+            }
         }
-        if(search(name)==true){
+    }
+    public void insert(String name, String phone) {
+        if (isFull() || (double) size / capacity > 0.7) { 
+            rehash();
+        }
+        if (search(name)) {
             System.out.println("The contact already exists");
             return;
         }
-        int index=(int)hash_calc(name);
-        int i=0;
-        while(Contacts[index] != null && Contacts[index].node != null){
-            index=(index+i*i) % capacity;
-            ++i;
+        int index = (int) hash_calc(name);
+        int i = 0;
+        while (Contacts[index] != null && Contacts[index].node != null) {
+            index = (index + i * i) % capacity;
+            i++;
         }
-        Contacts[index]= new HashNode(name, phone);
-        System.out.println("The contact is added successfulty.");
+        Contacts[index] = new HashNode(name, phone);
+        System.out.println("The contact is added successfully.");
         size++;
     }
-    public void remove(String name){
-        int index=(int)hash_calc(name);
-        int i=0;
-        boolean found=false;
-        while(Contacts[index] != null && Contacts[index].node != null){
-            if(Contacts[index].node.name.equalsIgnoreCase(name)&& Contacts[index].occupiedBefore){
-                Contacts[index].node=null;
-                Contacts[index].occupiedBefore=false;
-                found=true;
-                System.out.println("The contact with name "+name+" is removed successfully.");
-                removecounter++;
+
+    public void remove(String name) {
+        int index = (int) hash_calc(name);
+        int i = 0;
+
+        if (!search(name)) {
+            System.out.println("The contact with name " + name + " doesn't exist.");
+            return;
+        }
+
+        while (Contacts[index] != null && Contacts[index].node != null) {
+            if (Contacts[index].node.name.equalsIgnoreCase(name) && Contacts[index].occupiedBefore) {
+                Contacts[index].node = null;
+                Contacts[index].occupiedBefore = false;
+                System.out.println("The contact with name " + name + " is removed successfully.");
                 size--;
                 break;
             }
-            index=(index+i*i) % capacity;
-            ++i;
-        }
-        if(found==false){
-            System.out.println("The contact with name "+name+" doesn't exist.");
+            index = (index + i * i) % capacity;
+            i++;
         }
     }
-    public boolean search(String name){
-        
-        
-        int index=(int)hash_calc(name);
-        int i=0;
-        while(Contacts[index] != null && Contacts[index].node != null){
-            if(Contacts[index].node.name.equalsIgnoreCase(name) && Contacts[index].occupiedBefore) {
-                System.out.println("found at index: "+index);
+
+    public boolean search(String name) {
+        int index = (int) hash_calc(name);
+        int i = 0;
+
+        while (Contacts[index] != null && Contacts[index].node != null) {
+            if (Contacts[index].node.name.equalsIgnoreCase(name) && Contacts[index].occupiedBefore) {
+                System.out.println("Found at index: " + index);
                 System.out.println("Phone: " + Contacts[index].node.phone);
                 return true;
             }
-            index=(index +i*i) % capacity;
-            ++i;
-        }
-        
-        return false;
-    }
-    public void update(String name, String newPhone){
-        int index=(int)hash_calc(name);
-        int i =0;
-
-        while(Contacts[index] != null && Contacts[index].node != null){
-            if(Contacts[index].node.name.equalsIgnoreCase(name) && Contacts[index].occupiedBefore) {
-                String oldPhone = Contacts[index].node.phone;
-                Contacts[index].node.phone = newPhone;
-                System.out.println("Contact updated from "+oldPhone+" to "+newPhone);
-                return;
-            }
-            index= (int) ((index + i*i) % capacity);
+            index = (index + i * i) % capacity;
             i++;
         }
-        
+        return false;
     }
-    void display(){
-        
-        System.out.println("----------------------------------");
-        for(int i=0; i<capacity; i++){
-            try{
-                System.out.println("Name: "+Contacts[i].node.name+"\nPhone: "+Contacts[i].node.phone);
-                System.out.println("At index: "+i);
-                System.out.println("----------------------------------");
-            } 
-            catch (NullPointerException e) {}
-            
-            }
-        System.out.println("You have "+size()+" Contacts.");
-    }
-        
-    }
-    
 
+    public void update(String name, String newPhone) {
+        int index = (int) hash_calc(name);
+        int i = 0;
+
+        while (Contacts[index] != null && Contacts[index].node != null) {
+            if (Contacts[index].node.name.equalsIgnoreCase(name) && Contacts[index].occupiedBefore) {
+                String oldPhone = Contacts[index].node.phone;
+                Contacts[index].node.phone = newPhone;
+                System.out.println("Contact updated from " + oldPhone + " to " + newPhone);
+                return;
+            }
+            index = (index + i * i) % capacity;
+            i++;
+        }
+    }
+
+    void display() {
+        System.out.println("----------------------------------");
+        for (int i = 0; i < capacity; i++) {
+            try {
+                System.out.println("Name: " + Contacts[i].node.name + "\nPhone: " + Contacts[i].node.phone);
+                System.out.println("At index: " + i);
+                System.out.println("----------------------------------");
+            } catch (NullPointerException e) {}
+        }
+        System.out.println("You have " + size() + " Contacts.");
+    }
+}
